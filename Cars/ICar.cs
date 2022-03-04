ca29c959f705cfb4f3a4cd1cf739a1e1f5fdb9b0
Ivan Stoyanov <86497265+Vanko888@users.noreply.github.com>
2022-03-04 02:16:05 +0200

    Add project files.

---
+++ Cars/ICar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        string Model { get; set; }
+        string Color { get; set; }
+
+        string Start()
+        {
+            return "Engine start";
+        }
+
+        string Stop()
+        {
+            return "Breaaak!";
+        }
+
+
+    }
+}
---
+++ Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        string Start()
+        {
+            return "Engine start";
+        }
+
+        string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
